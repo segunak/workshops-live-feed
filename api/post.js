08a1c60e258d72bcbf0c8fb9fc6961e19b96eb34
Segun Akinyemi <segunak@microsoft.com>
2026-01-27 23:14:06 -0500

    Update Airtable post request to include typecasting and enhance footer styling

--- api/post.js
+++ api/post.js
@@ -69,7 +69,7 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ fields })
+      body: JSON.stringify({ fields, typecast: true })
     });
 
     if (response.ok) {
